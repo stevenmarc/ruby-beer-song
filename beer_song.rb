97f7e15b2ed649038a6c62fb5f6f8c89a1ccb9b1
Steven-Marc <stevenmarc@macbook-pro.home>
2017-02-15 21:44:18 +0100

    passes first test

--- beer_song.rb
+++ beer_song.rb
@@ -1,8 +1,8 @@
 class BeerSong
 
-def verse
-
-end
-
+	def verse(n)
+		"#{ n } bottles of beer on the wall, #{ n } bottles of beer.\n" \
+		"Take one down and pass it around, #{ n - 1 } bottles of beer on the wall.\n" 
+	end
 
 end
